7cb0452e512cd4f2052e3af6d47b767fc77f4eaf
Mikhail <mixandoma@ya.ru>
2016-11-07 01:26:03 +0500

    send form creation + 2 views added

--- app.rb
+++ app.rb
@@ -4,5 +4,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!"
+end
+
+get '/forum' do
+	erb :forum
+end
+
+get '/send' do
+	erb :send
+end
+
+post '/send' do
+  @message = params[:message]
+	erb @message
 end
