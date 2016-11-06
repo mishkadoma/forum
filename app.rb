7f8134429d66048c3b6b4ec94b09bb8d651025c1
Mikhail <mixandoma@ya.ru>
2016-11-07 01:44:41 +0500

    implementation гостевая книга

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!"
 end
@@ -17,5 +19,5 @@ end
 
 post '/send' do
   @message = params[:message]
-	erb @message
+	erb :send
 end
